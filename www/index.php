7d4a8ff49c61de0e2d8b945b03e9e83d7086b082
vijayy <vijay@admarvel.com>
2015-11-04 19:56:08 +0530

    initial commit

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<?php echo "<h1>Hello Vijay!!!</h1>"; ?>
+
+
+<?
+
+if(true) {
+	echo "<div>Docker is awesome!!!asdljasld</div>";
+}
+
+
+
+?>
+
+<h2>docker run -p 8081:80 -d  -v /Users/vijayy/Sites/map-setup/www:/var/www/site map-setup</h2>
